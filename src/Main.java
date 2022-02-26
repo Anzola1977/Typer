617a4ef3426fbdf7e37636e4c3844bae6b63f603
Andrei Zola <anzola@yandex.ru>
2022-02-26 16:08:01 +0100

    Next commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,14 @@ public class Main {
         Typer typer = new Typer("Message first. ");
         Typer typer1 = new Typer("Message second.");
         typer.start();
-        typer1.start();
         try {
             typer.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        typer1.start();
+        try {
+            typer1.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
--- src/Typer.java
+++ src/Typer.java
@@ -18,3 +18,4 @@ public class Typer extends Thread {
             }
         }
     }
+
